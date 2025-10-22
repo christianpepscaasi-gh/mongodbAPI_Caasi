19e0faf585d8077ba492b7ca683a75ba76e00f20
Ralph Pinlac <ralphadrianpinlac@gmail.com>
2025-10-22 16:35:14 +0800

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const Product = require('./models/Product');
+const validateProduct = require('./middlware/validateProduct');
+
+//Connect to MongoDB
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: "Hello World - Api is up!"});
+    res.json({ message: "Hello World - Api is up!" });
 });
 
+//Create a new product
+app.post('/api/products', validateProduct, async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        const savedProduct = await product.save();
+        res.status(201).json(savedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
